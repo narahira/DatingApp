c0c98d2fd36b450366d2885304ec05a31eb5c66e
Hariharan.V <hariharan6399@gmail.com>
2023-10-19 11:39:38 +0530

    DatingApp

---
+++ --SQLite.sql
@@ -0,0 +1,10 @@
+-- SQLite
+INSERT INTO Users (Id, Name)
+VALUES (1, "Shiva");
+
+
+INSERT INTO Users (Id, Name)
+VALUES (2, "Hari");
+
+INSERT INTO Users (Id, Name)
+VALUES (3, "Bala");
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    
+    public class AppUser
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
